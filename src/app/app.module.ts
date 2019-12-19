b91277fdb5ccd01b6919547b802484d597e715bb
mayde24 <maya.douadi@outlook.fr>
2019-12-19 09:47:43 +0100

    Perfectionnist mode activated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AgmCoreModule } from '@agm/core';
+import { NgsRevealModule } from 'ngx-scrollreveal';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgsRevealModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyC3PS0DwWZZetsoMDIhXJR3GjM09MwMjxw',
+      libraries: ['places']
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  map: mapboxgl.Map;
+  style = 'mapbox://styles/mayde24/ck4c0b6sj014a1dnu9wrvu8wm';
+  lat = 48.851660;
+  lng = 2.349353;
   constructor() { }
 
   ngOnInit() {
+    (mapboxgl as typeof mapboxgl).accessToken = 'pk.eyJ1IjoibWF5ZGUyNCIsImEiOiJjanl0eXZjZnMwNjQwM2RueWQwYzZ2NGw4In0.OuuJ8vXCOEqpm5jtApUJrw';
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      zoom: 15,
+      center: [this.lng, this.lat]
+    });
+    // Add map controls
+    this.map.addControl(new mapboxgl.AttributionControl(), 'top-left');
   }
-
 }
